6a63e7d40e2c7ed515dbe55cec1f8640622d2cc0
David Urbano <david.urbano_code@outlook.com>
2022-05-26 20:20:45 -0500

    Terminando el header

---
+++ Scripts/Version.js
@@ -0,0 +1,25 @@
+let texto = document.getElementById("version")
+
+const Actual = new Date()
+const Birth = new Date("2003-12-08")
+
+
+let ObtenerTiempo = () => {
+  let edadAnios =  Actual.getFullYear() - Birth.getFullYear()
+  let ActualMes = Actual.getMonth() + 1
+  let edadMes =  (Birth.getMonth() - ActualMes) + 1
+  let edadDia =  Actual.getDate() - Birth.getDate() 
+  
+  if(!(Actual.getMonth + 1 == 12) && !(Actual.getDate >= 8)){
+    edadAnios = edadAnios - 1
+  }
+  if(Actual.getDate <8){
+    ActualMes = ActualMes - 1
+  }
+  /* return `${edadAnios}.${ActualMes}.${edadDia}` */
+  return `v${edadAnios}.${ActualMes}`
+}
+
+ObtenerTiempo()
+dato = ObtenerTiempo()
+texto.innerText = dato
