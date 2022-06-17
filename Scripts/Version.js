4d371174f2b2a5f4bb14ed7c166e164007fc3f3f
David Urbano <david.urbano_code@outlook.com>
2022-06-17 13:42:36 -0500

    aun jugando con el sitio web

--- Scripts/Version.js
+++ Scripts/Version.js
@@ -1,25 +1,19 @@
-let texto = document.getElementById("version")
+/* eslint-disable operator-assignment */
+/* eslint linebreak-style: ["error", "windows"] */
+const texto = document.getElementById('version');
+const Actual = new Date();
+const Birth = new Date('2003-12-08');
 
-const Actual = new Date()
-const Birth = new Date("2003-12-08")
-
-
-let ObtenerTiempo = () => {
-  let edadAnios =  Actual.getFullYear() - Birth.getFullYear()
-  let ActualMes = Actual.getMonth() + 1
-  let edadMes =  (Birth.getMonth() - ActualMes) + 1
-  let edadDia =  Actual.getDate() - Birth.getDate() 
-  
-  if(!(Actual.getMonth + 1 == 12) && !(Actual.getDate >= 8)){
-    edadAnios = edadAnios - 1
-  }
-  if(Actual.getDate <8){
-    ActualMes = ActualMes - 1
-  }
+const ObtenerTiempo = () => {
+  let edadAnios = Actual.getFullYear() - Birth.getFullYear();
+  let ActualMes = Actual.getMonth() + 1;
+  /* let edadMes = (Birth.getMonth() - ActualMes) + 1;
+  let edadDia = Actual.getDate() - Birth.getDate(); */
+  if (!(Actual.getMonth + 1 === 12) && !(Actual.getDate >= 8)) { edadAnios = edadAnios - 1; }
+  if (Actual.getDate < 8) { ActualMes -= ActualMes; }
   /* return `${edadAnios}.${ActualMes}.${edadDia}` */
-  return `v${edadAnios}.${ActualMes}`
-}
-
-ObtenerTiempo()
-dato = ObtenerTiempo()
-texto.innerText = dato
+  return `v${edadAnios}.${ActualMes}`;
+};
+ObtenerTiempo();
+const dato = ObtenerTiempo();
+texto.innerText = dato;
